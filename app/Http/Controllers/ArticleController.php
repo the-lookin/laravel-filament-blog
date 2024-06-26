512ee1bf6a73a0c1e5acdccd2ca1f53daf79e70b
the-lookin <hitechnt@gmail.com>
2024-06-26 15:40:42 +0300

    Init

---
+++ app/Http/Controllers/ArticleController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ArticleController extends Controller
+{
+    public function index() {
+        return 'index article';
+    }
+
+    public function category($category) {
+        return 'category article ' . $category;
+    }
+
+    public function show($category, $article) {
+        return 'show article ' . $category . ' ' . $article;
+    }
+
+    public function tag($tag) {
+        return 'tag article ' . $tag;
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,14 @@
+<?php
+
+use App\Http\Controllers\ArticleController;
+use App\Http\Controllers\HomeController;
+use Illuminate\Support\Facades\Route;
+
+
+Route::get('/', [HomeController::class, 'index'])->name('home');
+Route::get('/blog', [ArticleController::class, 'index'])->name('article.index');
+Route::get('/blog/{category}', [ArticleController::class, 'category'])->name('article.category');
+Route::get('/blog/{category}/{article}', [ArticleController::class, 'show'])->name('article.show');
+Route::get('/blog/tag/{tag}', [ArticleController::class, 'tag'])->name('article.tag');
+
+Route::get('/about', [HomeController::class, 'about'])->name('about');
